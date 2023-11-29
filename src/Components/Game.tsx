6dcea67556324f2b5c1c0ad7a5abf0dab1e0ffaa
shimupan <shimupan@gmail.com>
2023-11-29 18:37:59 -0500

    Using links instead of navigation

--- src/Components/Game.tsx
+++ src/Components/Game.tsx
@@ -2,16 +2,19 @@
 import { Link } from 'react-router-dom';
 
 type GameProps = {
+  game: string;
   name: string;
 };
 
-const Game: React.FC<GameProps> = ( { name } ) => {
+const Game: React.FC<GameProps> = ( { game, name } ) => {
 
   return (
     <>
       <div className="flex flex-row items-center justify-center space-x-4">
         <div className="w-64 h-128">
-          <img className="w-full h-full object-cover cursor-pointer transform transition-all duration-500 hover:scale-105 shadow-lg hover:shadow-2xl" src={name} alt={name} />
+          <Link to={game}>
+            <img className="w-full h-full object-cover cursor-pointer transform transition-all duration-500 hover:scale-105 shadow-lg hover:shadow-2xl" src={name} alt={game} />
+          </Link>
         </div>
       </div>
     </>
--- src/Pages/Page.tsx
+++ src/Pages/Page.tsx
@@ -1,7 +1,7 @@
 import { Header, Game } from "../Components";
 
-import VALORANT from '../assets/valorant.jpg';
-import CS2 from '../assets/csgo2.webp';
+import cs2 from '../assets/csgo2.webp';
+import valorant from '../assets/valorant.jpg';
 
 const Page = () => {
 
@@ -9,8 +9,8 @@ const Page = () => {
       <>
          <Header />
          <div className="h-screen flex flex-row gap-4 justify-center items-center">
-            <Game name={CS2}/>
-            <Game name={VALORANT}/>
+            <Game game={"CS2"} name={cs2}/>
+            <Game game={"Valorant"} name={valorant}/>
          </div>
       </>
    );
