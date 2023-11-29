d9266b674704c6724b346685e50f2e642b4c98da
Derek Lee <37049496+Kagiri2@users.noreply.github.com>
2023-11-29 00:25:32 -0500

    added effects

--- src/App.tsx
+++ src/App.tsx
@@ -13,11 +13,11 @@ function App() {
     <>
       <img className="fixed top-0 left-1/2 transform -translate-x-1/2 w-96 h-auto" src={"src/assets/lineupx.png"} alt="Logo" />
       <div className="flex flex-row items-center justify-center space-x-4">
-        <div className={`w-48 h-128 border-2 ${selectedButton === 1 ? 'border-blue-500' : 'border-transparent'}`}>
-          <img className="w-full h-full object-cover cursor-pointer" src={"src/assets/csgo2.webp"} alt="Button 1" onClick={() => handleClick(1)} />
+        <div className="w-48 h-128">
+          <img className="w-full h-full object-cover cursor-pointer transform transition-all duration-500 hover:scale-105 shadow-lg hover:shadow-2xl" src={"src/assets/csgo2.webp"} alt="Button 1" onClick={() => handleClick(1)} />
         </div>
-        <div className={`w-48 h-128 border-2 ${selectedButton === 2 ? 'border-blue-500' : 'border-transparent'}`}>
-          <img className="w-full h-full object-cover cursor-pointer" src={"src/assets/valorant.jpg"} alt="Button 2" onClick={() => handleClick(2)} />
+        <div className="w-48 h-128">
+          <img className="w-full h-full object-cover cursor-pointer transform transition-all duration-500 hover:scale-105 shadow-lg hover:shadow-2xl" src={"src/assets/valorant.jpg"} alt="Button 2" onClick={() => handleClick(2)} />
         </div>
       </div>
     </>
