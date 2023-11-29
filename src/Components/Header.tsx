dc3ef35e40f2fd5baf37fafa18193919f32eb08a
shimupan <shimupan@gmail.com>
2023-11-29 00:20:00 -0500

    fixed logo

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,8 +1,10 @@
+import logo from '../assets/lineupx.png';
+
 const Header = () => {
 
   return (
     <div>
-      <img className="fixed top-0 left-1/2 transform -translate-x-1/2 w-96 h-auto" src="./src/assets/lineupx.png" alt="Logo" />
+      <img className="fixed top-0 left-1/2 transform -translate-x-1/2 w-96 h-auto" src={logo} alt="Logo" />
     </div>
   );
 };
