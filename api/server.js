fe835cddd76659f559d483ac6e93dae019b6d475
shimupan <shimupan@gmail.com>
2023-11-28 19:16:11 -0500

    setup fonts

--- api/server.js
+++ api/server.js
@@ -7,3 +7,9 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+const port = process.env.PORT || 8080;
+
+const server = app.listen(port, () => {
+    console.log(`Server running on port ${port}`)
+});
