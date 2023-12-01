80e7d6027b8b1cfe41e78fbd974ef577680e0be9
Derek Lee <37049496+Kagiri2@users.noreply.github.com>
2023-11-30 20:49:09 -0500

    Update Header.tsx

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -16,7 +16,6 @@ const Header = () => {
                       <img src={logo} alt="Logo" className="w-100 h-20 transform scale-125" />
                     </Link>
                   </li>
-                  <li><a className="inline-block no-underline hover:text-black font-medium text-lg py-2 px-4 lg:-ml-2" href="#">Home</a></li>
                   <li><a className="inline-block no-underline hover:text-black font-medium text-lg py-2 px-4 lg:-ml-2" href="#">Products</a></li>
                   <li><a className="inline-block no-underline hover:text-black font-medium text-lg py-2 px-4 lg:-ml-2" href="#">About</a></li>
               </ul>
@@ -25,8 +24,8 @@ const Header = () => {
         
         <div className="order-2 md:order-3 flex flex-wrap items-center justify-end mr-0 md:mr-4" id="nav-content">
             <div className="auth flex items-center w-full md:w-full">
-              <Link to={"/login"} className="bg-transparent text-gray-800  p-2 rounded border border-gray-300 mr-4 hover:bg-gray-100 hover:text-gray-700">Sign in</Link>
-              <Link to={"/register"} className="bg-blue-600 text-gray-200  p-2 rounded  hover:bg-blue-500 hover:text-gray-100">Sign up</Link>
+              <Link to={"/login"} className="bg-gray-600 text-white p-2 rounded border border-gray-700 mr-4 hover:bg-gray-500 hover:text-white">Sign in</Link>
+              <Link to={"/register"} className="bg-gray-600 text-white p-2 rounded hover:bg-gray-500 hover:text-white">Sign up</Link>
             </div>
         </div>
       </div>
