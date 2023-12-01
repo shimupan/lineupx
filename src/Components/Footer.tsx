fa051cdb50e67175ab05319920e8872d1f601cf6
Derek Lee <dereklee675@gmail.com>
2023-12-01 01:06:53 -0500

    22 better main page (#26)
    * fixed header
    
    * footer
    
    * Update Header.tsx
    
    * added some gradient
    
    * added purple

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,10 +1,34 @@
-const Footer = () => {
+import React from 'react';
 
-  return (
-    <div>
-      <h1>Footer</h1>
-    </div>
-  );
+const Footer: React.FC = () => {
+    return (
+        <footer className="bg-gray-200 text-center lg:text-left">
+            <div className="container p-6">
+                <div className="flex justify-center items-center lg:justify-between">
+                    <div className="text-gray-700">
+                        <span>© {new Date().getFullYear()} Your Company Name. All rights reserved.</span>
+                    </div>
+                    <div className="flex justify-center">
+                        <a href="#!" className="mr-6 text-gray-600">
+                            <i className="fab fa-facebook-f"></i>
+                        </a>
+                        <a href="#!" className="mr-6 text-gray-600">
+                            <i className="fab fa-twitter"></i>
+                        </a>
+                        <a href="#!" className="mr-6 text-gray-600">
+                            <i className="fab fa-google-plus-g"></i>
+                        </a>
+                        <a href="#!" className="mr-6 text-gray-600">
+                            <i className="fab fa-linkedin-in"></i>
+                        </a>
+                        <a href="#!" className="text-gray-600">
+                            <i className="fab fa-instagram"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    );
 };
- 
- export default Footer;
+
+export default Footer;
