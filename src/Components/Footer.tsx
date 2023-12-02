283296012ec1f0e0ab5c710ddc5434e3f2e69192
Derek Lee <37049496+Kagiri2@users.noreply.github.com>
2023-12-02 16:28:34 -0500

    footer

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,34 +1,35 @@
 import React from 'react';
-
+import github from '../assets/github.png';
 interface FooterProps {
     className?: string;
 }
 
 const Footer: React.FC<FooterProps> = ({ className }) => {
     return (
-        <footer className={`bg-gray-200 text-center lg:text-left ${className}`}>
-            <div className="container p-6">
-                <div className="flex justify-center items-center lg:justify-between">
-                    <div className="text-gray-700">
-                        <span>© {new Date().getFullYear()} LineupX. All rights reserved.</span>
-                    </div>
-                    <div className="flex justify-center">
-                        <a href="#!" className="mr-6 text-gray-600">
-                            <i className="fab fa-facebook-f"></i>
-                        </a>
-                        <a href="#!" className="mr-6 text-gray-600">
-                            <i className="fab fa-twitter"></i>
-                        </a>
-                        <a href="#!" className="mr-6 text-gray-600">
-                            <i className="fab fa-google-plus-g"></i>
-                        </a>
-                        <a href="#!" className="mr-6 text-gray-600">
-                            <i className="fab fa-linkedin-in"></i>
-                        </a>
-                        <a href="#!" className="text-gray-600">
-                            <i className="fab fa-instagram"></i>
-                        </a>
-                    </div>
+        <footer className={`bg-white text-center lg:text-left fixed bottom-0 w-full ${className}`}>
+            <div className="container p-6 flex justify-end items-center">
+                <div className="text-gray-700 pr-80">
+                    <span>{new Date().getFullYear()} LineupX. All rights reserved.
+                    <a href="https://github.com/yourusername" className="mr-6">
+                        <img src={github} alt="GitHub" width="24" height="24" />
+                    </a></span>
+                </div>
+                <div className="flex justify-center">
+                    <a href="#!" className="mr-6 text-gray-600">
+                        <i className="fab fa-facebook-f"></i>
+                    </a>
+                    <a href="#!" className="mr-6 text-gray-600">
+                        <i className="fab fa-twitter"></i>
+                    </a>
+                    <a href="#!" className="mr-6 text-gray-600">
+                        <i className="fab fa-google-plus-g"></i>
+                    </a>
+                    <a href="#!" className="mr-6 text-gray-600">
+                        <i className="fab fa-linkedin-in"></i>
+                    </a>
+                    <a href="#!" className="text-gray-600">
+                        <i className="fab fa-instagram"></i>
+                    </a>
                 </div>
             </div>
         </footer>
