0cc22056263f280b25c682ccfebaa82de321c392
Derek Lee <37049496+Kagiri2@users.noreply.github.com>
2023-12-02 18:25:02 -0500

    added files

---
+++ api/model/mailer.js
@@ -0,0 +1,19 @@
+// mailer.js
+import nodemailer from 'nodemailer';
+
+const transporter = nodemailer.createTransport({
+  service: 'Gmail', // e.g., Gmail, Outlook, etc.
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
+
+export const sendEmail = async (to, subject, html) => {
+  try {
+    await transporter.sendMail({ from: process.env.EMAIL_USER, to, subject, html });
+    console.log('Email sent successfully');
+  } catch (error) {
+    console.error('Error sending email:', error);
+  }
+};
--- api/routes/register.js
+++ api/routes/register.js
@@ -1,32 +1,44 @@
 import express from 'express';
-
 import User from '../model/user.js';
 import { signInAccessToken, refreshAccessToken } from '../helper/jwtHelper.js';
+import { sendEmail } from '../model/mailer.js'; 
 
 const router = express.Router();
 
 router.post('/register', async (req, res) => {
-   const { email, password } = req.body;
-   const existingUser = await User.findOne({ email });
-   if (existingUser) {
-      return res.status(400).send({ message: 'Email already in use' });
-   }
+  const { email, password } = req.body;
+  const existingUser = await User.findOne({ email });
+  if (existingUser) {
+    return res.status(400).send({ message: 'Email already in use' });
+  }
 
-   try {
-      const user = new User({
-         email: email,
-         password: password,
-         likes: [],
-         dislikes: [],
-         saved: []
-      });
-      const newUser = await user.save();
-      const accessToken = await signInAccessToken(newUser.id);
-      const refreshToken = await refreshAccessToken(newUser.id);
-      res.send({accessToken, refreshToken});
-   } catch (error) {
-      res.status(400).send({ message: 'Error registering user', error: error.message });
-   }
+  try {
+    const user = new User({
+      email: email,
+      password: password,
+      likes: [],
+      dislikes: [],
+      saved: []
+    });
+
+    // Generate email verification token
+    const emailVerificationToken = user.generateVerificationToken();
+    
+    // Save the user with the verification token
+    const newUser = await user.save();
+
+    // Send email verification link
+    const verificationUrl = `http://<your-domain>/verify-email/${emailVerificationToken}`;
+    await sendEmail(email, 'Verify Your Email', `Please click on the following link to verify your email: <a href="${verificationUrl}">${verificationUrl}</a>`);
+
+    // Generate access and refresh tokens
+    const accessToken = await signInAccessToken(newUser.id);
+    const refreshToken = await refreshAccessToken(newUser.id);
+
+    res.send({ accessToken, refreshToken });
+  } catch (error) {
+    res.status(400).send({ message: 'Error registering user', error: error.message });
+  }
 });
- 
+
 export default router;
