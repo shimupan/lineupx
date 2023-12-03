aafc5414b71ceddb8a8c47cd8f1c9a4e1e5df53a
Derek Lee <37049496+Kagiri2@users.noreply.github.com>
2023-12-02 19:18:11 -0500

    fixed

--- api/model/mailer.js
+++ api/model/mailer.js
@@ -1,17 +1,18 @@
-// mailer.js
 import nodemailer from 'nodemailer';
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail', // e.g., Gmail, Outlook, etc.
+  host: 'smtp.zoho.com',
+  port: 465, // Use 587 if you want to connect using STARTTLS
+  secure: true, // true for 465, false for other ports
   auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
+    user: 'username@example.com',
+    pass: 'password'
   },
 });
 
 export const sendEmail = async (to, subject, html) => {
   try {
-    await transporter.sendMail({ from: process.env.EMAIL_USER, to, subject, html });
+    await transporter.sendMail({ from: 'lineupx@lineupx.net', to, subject, html });
     console.log('Email sent successfully');
   } catch (error) {
     console.error('Error sending email:', error);
