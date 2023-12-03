91beed670b1e44427dae283681f9de7c21964351
Derek Lee <37049496+Kagiri2@users.noreply.github.com>
2023-12-02 20:35:45 -0500

    Update index.js

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,4 +1,3 @@
 export { default as login } from './login.js';
 export { default as logout } from './logout.js';
 export { default as register } from './register.js';
-export { default as authRoutes } from './authRoutes.js';
