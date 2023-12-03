30fa70bdf51329214b2016c3aeb48c112d9735d0
shimupan <shimupan@gmail.com>
2023-12-03 01:24:22 -0500

    Build errors

--- src/Pages/ProfilePage.tsx
+++ src/Pages/ProfilePage.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useContext } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { Header, SideNavWrapper, Footer } from "../Components";
 
