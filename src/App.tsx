2fb441ab7c43132d9e93c34a2bd34ded79f50dd7
Derek Lee <37049496+Kagiri2@users.noreply.github.com>
2023-12-03 02:08:12 -0500

    test done moved files

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect, createContext } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Page, UserPage, Valorant, CS2, Register, Login } from "./Components";
+import { setupInterceptors } from './axiosConfig';
 import axios from 'axios';
 import Cookies from "universal-cookie";
 import './App.css'
@@ -21,45 +22,7 @@ type AuthContextType = {
 
 export const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
-// Request Interceptor
-axios.interceptors.request.use(request => {
-  // Add the access token to every request
-  const accessToken = cookies.get('accessToken');
-  if (accessToken) {
-    request.headers['Authorization'] = `Bearer ${accessToken}`;
-  }
-  return request;
-}, error => {
-  return Promise.reject(error);
-});
-
-// Response Interceptor
-axios.interceptors.response.use(response => {
-  return response;
-}, async error => {
-  const originalRequest = error.config;
-
-  // Check for token expiration (401 status)
-  if (error.response.status === 401 && !originalRequest._retry) {
-    originalRequest._retry = true;
-    const refreshToken = cookies.get('refreshToken');
-
-    // Attempt to get a new access token using refresh token
-    return axios.post('/refresh-token', { refreshToken })
-      .then(res => {
-        if (res.status === 200) {
-          // Update the access token in cookies
-          cookies.set('accessToken', res.data.accessToken);
-
-          // Update the header and resend the original request
-          axios.defaults.headers.common['Authorization'] = `Bearer ${res.data.accessToken}`;
-          return axios(originalRequest);
-        }
-      });
-  }
-
-  return Promise.reject(error);
-});
+setupInterceptors();
 
 function App() {
   const [accessToken, setAccessToken] = useState<string>('');
