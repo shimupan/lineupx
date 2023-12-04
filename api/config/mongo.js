730cfce8a93ab59d3f3ae1f1c414e8d1f1089c31
shimupan <shimupan@gmail.com>
2023-12-04 17:21:15 -0500

    testing nodejs action

--- api/config/mongo.js
+++ api/config/mongo.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-const mongo = () => {mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+const mongo = () => {mongoose.connect(process.env.MONGO_URI || "mongodb://localhost:27017/LineupX", { useNewUrlParser: true, useUnifiedTopology: true })
   .then(async (db) => {
     console.log('database connected');
     const collections = await db.connection.db.listCollections().toArray();
