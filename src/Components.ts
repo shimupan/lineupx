bf436cadcc80d2698d26237dbc78dafae51ad2c2
Derek Lee <37049496+Kagiri2@users.noreply.github.com>
2023-12-03 22:37:34 -0500

    fixed

--- src/Components.ts
+++ src/Components.ts
@@ -5,7 +5,7 @@ export {default as Valorant} from './Pages/game/Valorant';
 export {default as CS2} from './Pages/game/CS2';
 export {default as Login} from './Pages/auth/Login';
 export {default as Register} from './Pages/auth/Register';
-export {default as ForgotPassword} from './Pages/auth/forgotPassword';
+export {default as ForgotPassword} from './Pages/auth/ForgotPassword';
 export {default as ResetPassword} from './Pages/auth/ResetPassword';
 
 // Components
