519602ef1f6955966fdc7dec3486cc01457d9295
shimupan <102918160+shimupan@users.noreply.github.com>
2023-12-09 13:34:39 -0500

    65 house keeping (#81)
    * Refactored code for api, making seperate routes
    
    * fixed routing errors
    
    * testing nodejs action
    
    * testing actions
    
    * added the api working directory
    
    * adjusting time
    
    * Added prettier and setting up actions for prettier
    
    * prettier action check
    
    * trying auto deploy
    
    * updated password
    
    * prettier change
    
    * updated condition

--- api/routes/post.js
+++ api/routes/post.js
@@ -2,12 +2,8 @@ import express from 'express';
 
 const router = express.Router();
 
-router.get('/post', (req, res) => {
+router.get('/post', (req, res) => {});
 
-});
-
-router.post('/post', (req, res) => {
-
-});
+router.post('/post', (req, res) => {});
 
 export default router;
