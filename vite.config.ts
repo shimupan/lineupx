6d59a83ad8710fbba1ddac4aada2c35064d9536f
shimupan <shimupan@gmail.com>
2023-12-17 02:00:56 -0500

    Added docker support for mongo and backend

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+   plugins: [react()],
+   server: {
+      watch: {
+         usePolling: true,
+      },
+      host: true, // needed for the Docker Container port mapping to work
+      strictPort: true,
+      port: 5173, // you can replace this port with any port
+   },
+});
