5c00b3e6069b9b4cd0cf5b7936de4df7b4b9692c
Derek Lee <37049496+Kagiri2@users.noreply.github.com>
2023-12-18 14:41:00 -0500

    text

--- src/web/src/Components/Searchbar.tsx
+++ src/web/src/Components/Searchbar.tsx
@@ -25,7 +25,7 @@ const SearchBar = ({
    return (
       <form
          onSubmit={handleSubmit}
-         className={`flex items-center border border-gray-300 rounded w-2/5 ${className}`}
+         className={`flex items-center rounded w-2/5 ${className}`}
       >
          <button type='submit'></button>
          <input
@@ -33,7 +33,7 @@ const SearchBar = ({
             placeholder={placeholder}
             value={searchTerm}
             onChange={handleChange}
-            className='p-2 flex-1 focus:outline-none rounded-r w-full' // Adjusted for length and rounded corners
+            className="flex text-black items-center w-full px-5 py-4 mr-2 text-sm font-medium outline-none focus:bg-grey-400 mb-7 placeholder:text-grey-700 bg-grey-200 text-dark-grey-900 rounded-2xl"
          />
       </form>
    );
