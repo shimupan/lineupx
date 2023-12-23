5ff63e9ecb3f84e476907e71d8ea8a7d38c1e098
Derek Lee <37049496+Kagiri2@users.noreply.github.com>
2023-12-23 01:24:44 -0500

    Update Posts.tsx

--- src/web/src/Components/Posts.tsx
+++ src/web/src/Components/Posts.tsx
@@ -7,22 +7,28 @@ interface PostsProps {
 
 const Posts: React.FC<PostsProps> = ({ postData }) => {
    return (
-      <>
-         <div>{postData.UserID}</div>
+      <div className="max-w-md mx-auto my-5 p-5 border border-gray-300 rounded-lg shadow-sm bg-white">
+         <div className="text-center text-lg font-bold text-gray-800 mb-4">
+            User ID: {postData.UserID}
+         </div>
          <img
+            className="w-full h-auto rounded mb-3"
             src={`${CDN_URL}/${postData.landingPosition.public_id}`}
-            alt="landing position"
+            alt="Landing Position"
          />
          <img
+            className="w-full h-auto rounded mb-3"
             src={`${CDN_URL}/${postData.standingPosition.public_id}`}
-            alt="aiming position"
+            alt="Standing Position"
          />
          <img
+            className="w-full h-auto rounded mb-3"
             src={`${CDN_URL}/${postData.aimingPosition.public_id}`}
-            alt="landing position"
+            alt="Aiming Position"
          />
-      </>
+      </div>
    );
 };
 
 export default Posts;
+
