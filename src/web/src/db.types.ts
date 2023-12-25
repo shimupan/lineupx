fdc4298ac5dee76010bd152c95928e5aea595d6e
shimupan <102918160+shimupan@users.noreply.github.com>
2023-12-24 19:10:28 -0500

    Valorant agent page (#114)
    * added some new components
    
    * new redirects
    
    * preparing for merge
    
    ---------
    
    Co-authored-by: Derek Lee <37049496+Kagiri2@users.noreply.github.com>

--- src/web/src/db.types.ts
+++ src/web/src/db.types.ts
@@ -33,3 +33,58 @@ export type PostType = {
    jumpThrow: string;
    game: string;
 };
+
+export type ValorantAgent = {
+   status: number;
+   data: {
+      uuid: string;
+      displayName: string;
+      description: string;
+      developerName: string;
+      characterTags: string[];
+      displayIcon: string;
+      displayIconSmall: string;
+      bustPortrait: string;
+      fullPortrait: string;
+      fullPortraitV2: string;
+      killfeedPortrait: string;
+      background: string;
+      backgroundGradientColors: string[];
+      assetPath: string;
+      isFullPortraitRightFacing: boolean;
+      isPlayableCharacter: boolean;
+      isAvailableForTest: boolean;
+      isBaseContent: boolean;
+      role: {
+         uuid: string;
+         displayName: string;
+         description: string;
+         displayIcon: string;
+         assetPath: string;
+      };
+      recruitmentData: {
+         counterId: string;
+         milestoneId: string;
+         milestoneThreshold: number;
+         useLevelVpCostOverride: boolean;
+         levelVpCostOverride: number;
+         startDate: Date;
+         endDate: Date;
+      };
+      abilities: {
+         slot: string;
+         displayName: string;
+         description: string;
+         displayIcon: string;
+         voiceLine: {
+            minDuration: number;
+            maxDuration: number;
+            mediaList: {
+               id: number;
+               wwise: string;
+               wave: string;
+            }[];
+         }[];
+      }[];
+   }[];
+};
