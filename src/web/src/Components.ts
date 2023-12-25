8d8bbcbcc1432d8adffbe12d20f1906c92f7109c
shimupan <shimupan@gmail.com>
2023-12-24 20:51:11 -0500

    testing admin panel

--- src/web/src/Components.ts
+++ src/web/src/Components.ts
@@ -15,6 +15,7 @@ export { default as CS2Lineups } from './Pages/game/CS2/CS2Lineups';
 export { default as ValorantLineups } from './Pages/game/Valorant/ValorantLineups';
 export { default as ValorantAgents } from './Pages/game/Valorant/ValorantAgents';
 export { default as ValorantMaps } from './Pages/game/Valorant/ValorantMapLineups';
+export { default as AdminHome } from './Pages/admin/AdminHome';
 // Components
 export { default as Header } from './Components/Header';
 export { default as Game } from './Components/Game';
--- src/web/src/Components/auth/RequireAuth.tsx
+++ src/web/src/Components/auth/RequireAuth.tsx
@@ -2,14 +2,20 @@ import { useContext } from 'react';
 import { useLocation, Navigate, Outlet } from 'react-router-dom';
 import { AuthContext } from '../../App';
 
-const RequireAuth = () => {
+type RequireAuthProps = {
+   allowedRoles?: string[];
+};
+
+const RequireAuth: React.FC<RequireAuthProps> = ({ allowedRoles }) => {
    const location = useLocation();
    const Auth = useContext(AuthContext);
-   console.log(Auth);
-   return Auth?.accessToken ? (
+   console.log(Auth?.role, allowedRoles);
+   return Auth?.role && allowedRoles?.includes(Auth?.role) ? (
       <Outlet />
+   ) : Auth?.role ? (
+      <Navigate to="/" state={{ from: location }} replace />
    ) : (
-      <Navigate to='/login' state={{ from: location }} replace />
+      <Navigate to="/login" state={{ from: location }} replace />
    );
 };
 
