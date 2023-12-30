4c473d8ae4359c2502d9bc74696d3b669ba6d462
Derek Lee <37049496+Kagiri2@users.noreply.github.com>
2023-12-30 18:40:45 -0500

    Update Valorant.tsx

--- src/web/src/Pages/game/Valorant.tsx
+++ src/web/src/Pages/game/Valorant.tsx
@@ -16,6 +16,7 @@ import { useLocalStorage } from '../../hooks';
 const Valorant: React.FC = () => {
    const [open, setOpen] = useState<boolean>(true);
    const [posts, setPosts] = useState<PostType[]>([]);
+   const [value, setValue] = useLocalStorage('valorantPopup', true);
    const [filteredPosts, setFilteredPosts] = useState<PostType[]>([]);
    const [searchTerm, setSearchTerm] = useState('');
    const [suggestions, setSuggestions] = useState<string[]>([]);
@@ -115,51 +116,58 @@ const Valorant: React.FC = () => {
    };
 
    return (
-      <div className="flex flex-col min-h-screen">
-         <Header />
-         <main className="flex-1">
-            <SideNavWrapper />
-            <div
-               className="flex flex-col items-center h-72 relative bg-center bg-no-repeat"
-               style={{
-                  backgroundImage: `url(${VALORANT_BANNER})`,
-                  backgroundSize: '100%',
-                  backgroundPosition: '90% 10%',
-               }}
-            >
-               <div className="absolute inset-0 bg-black bg-opacity-50"></div>
-               <h1 className="text-lg mb-4 pt-10 font-bold z-10">Valorant</h1>
+      <>
+         {value && open && (
+            <ValorantPopup setOpen={setOpen} setValue={setValue} />
+         )}
+         <div className="flex flex-col min-h-screen">
+            <Header />
+            <main className="flex-1">
+               <SideNavWrapper />
+               <div
+                  className="flex flex-col items-center h-72 relative bg-center bg-no-repeat"
+                  style={{
+                     backgroundImage: `url(${VALORANT_BANNER})`,
+                     backgroundSize: '100%',
+                     backgroundPosition: '90% 10%',
+                  }}
+               >
+                  <div className="absolute inset-0 bg-black bg-opacity-50"></div>
+                  <h1 className="text-lg mb-4 pt-10 font-bold z-10">
+                     Valorant
+                  </h1>
 
-               <Searchbar
-                  onChange={(e) => handleSearch(e.target.value)}
-                  onSearch={handleSearch}
-                  placeholder="Search for Valorant Lineups"
-                  className="z-10"
-                  suggestions={suggestions}
-               />
-            </div>
-            <div className="flex flex-col items-center pt-5 pb-5 bg-black bg-opacity-50 backdrop-blur-md">
-               <div className="w-1/2">
-                  <Carousel images={VALORANT_MAPS} />
+                  <Searchbar
+                     onChange={(e) => handleSearch(e.target.value)}
+                     onSearch={handleSearch}
+                     placeholder="Search for Valorant Lineups"
+                     className="z-10"
+                     suggestions={suggestions}
+                  />
                </div>
-            </div>
-            {/* TODO: STYLING BELOW */}
+               <div className="flex flex-col items-center pt-5 pb-5 bg-black bg-opacity-50 backdrop-blur-md">
+                  <div className="w-1/2">
+                     <Carousel images={VALORANT_MAPS} />
+                  </div>
+               </div>
+               {/* TODO: STYLING BELOW */}
 
-            <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4 pl-20">
-               {(searchTerm === '' ? posts.slice(0, 10) : filteredPosts).map(
-                  (post) => (
-                     <div
-                        key={post.landingPosition.public_id}
-                        className="max-w-md mx-auto"
-                     >
-                        <Posts postData={post} />
-                     </div>
-                  ),
-               )}
-            </div>
-         </main>
-         <Footer className="mt-auto" />
-      </div>
+               <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4 pl-20">
+                  {(searchTerm === '' ? posts.slice(0, 10) : filteredPosts).map(
+                     (post) => (
+                        <div
+                           key={post.landingPosition.public_id}
+                           className="max-w-md mx-auto"
+                        >
+                           <Posts postData={post} />
+                        </div>
+                     ),
+                  )}
+               </div>
+            </main>
+            <Footer className="mt-auto" />
+         </div>
+      </>
    );
 };
 
