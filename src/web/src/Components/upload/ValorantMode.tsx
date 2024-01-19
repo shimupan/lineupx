dd9e62869725bf3611e470c5190e1e9a14a614a3
Derek Lee <37049496+Kagiri2@users.noreply.github.com>
2024-01-19 13:39:35 -0500

    finished Valorant but it keeps blinking

--- src/web/src/Components/upload/ValorantMode.tsx
+++ src/web/src/Components/upload/ValorantMode.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState, useRef } from 'react';
 import { StateVariables, StateActions } from '../../Pages/upload/upload.types';
 import { AgentSelector } from '../../Components';
-import axios from 'axios';
 
 import splitCoordinates from '../../assets/valorantjsons/split.json';
 import bindCoordinates from '../../assets/valorantjsons/bind.json';
@@ -13,6 +12,19 @@ import ascentCoordinates from '../../assets/valorantjsons/ascent.json';
 import pearlCoordinates from '../../assets/valorantjsons/pearl.json';
 import havenCoordinates from '../../assets/valorantjsons/haven.json';
 import iceboxCoordinates from '../../assets/valorantjsons/icebox.json';
+
+const mapData = {
+   split: { name: 'Split', coordinates: splitCoordinates.coordinates },
+   haven: { name: 'Haven', coordinates: havenCoordinates.coordinates },
+   bind: { name: 'Bind', coordinates: bindCoordinates.coordinates },
+   ascent: { name: 'Ascent', coordinates: ascentCoordinates.coordinates },
+   icebox: { name: 'Icebox', coordinates: iceboxCoordinates.coordinates },
+   breeze: { name: 'Breeze', coordinates: breezeCoordinates.coordinates },
+   fracture: { name: 'Fracture', coordinates: fractureCoordinates.coordinates },
+   sunset: { name: 'Sunset', coordinates: sunsetCoordinates.coordinates },
+   lotus: { name: 'Lotus', coordinates: lotusCoordinates.coordinates },
+   pearl: { name: 'Pearl', coordinates: pearlCoordinates.coordinates },
+};
 
 type ValorantModeProps = {
    state: StateVariables;
@@ -27,24 +39,32 @@ interface Map {
 
 const ValorantMode: React.FC<ValorantModeProps> = ({ state, dispatch }) => {
    const [maps, setMaps] = useState<Map[]>([]);
-   const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
    const canvasRef = useRef<HTMLCanvasElement | null>(null);
    const [clickPosition, setClickPosition] = useState<{
       x: number;
       y: number;
    } | null>(null);
+   const [hoverPosition, setHoverPosition] = useState<{
+      x: number;
+      y: number;
+   } | null>(null);
+   const [coordinates, setCoordinates] = useState<
+      { x: number; y: number; name: string }[]
+   >([]);
+   const [selectedDot, setSelectedDot] = useState<{
+      x: number;
+      y: number;
+      name: string;
+   } | null>(null);
+   const [placedDot, setPlacedDot] = useState<{ x: number; y: number } | null>(
+      null,
+   );
 
    const selectedMap = maps.find(
       (map) => map.displayName.toLowerCase() === state.mapName.toLowerCase(),
    );
 
    useEffect(() => {
-
-      const handleResize = () => {
-         setIsMobile(window.innerWidth <= 768);
-      };
-      window.addEventListener('resize', handleResize);
-
       fetch('https://valorant-api.com/v1/maps')
          .then((response) => response.json())
          .then((data) => setMaps(data.data));
@@ -56,57 +76,71 @@ const ValorantMode: React.FC<ValorantModeProps> = ({ state, dispatch }) => {
       if (canvas && selectedMap) {
          const context = canvas.getContext('2d');
          const img = new Image();
+         img.crossOrigin = "anonymous";
 
          img.onload = () => {
             canvas.width = img.width * 2;
             canvas.height = img.height * 2;
             context?.drawImage(img, 0, 0, img.width * 2, img.height * 2);
-            if (clickPosition && context) {
-               context.beginPath();
-               context.arc(
-                  clickPosition.x,
-                  clickPosition.y,
-                  10,
-                  0,
-                  2 * Math.PI,
-                  false,
-               );
-               context.fillStyle = 'red';
-               context.fill();
+
+            coordinates.forEach((coord) => {
+               if (context) {
+                  const color =
+                     hoverPosition &&
+                     Math.hypot(
+                        coord.x - hoverPosition.x,
+                        coord.y - hoverPosition.y,
+                     ) < 15
+                        ? 'red'
+                        : 'blue';
+                  drawMarker(context, coord.x, coord.y, 15, color);
+               }
+            });
+            if (context) {
+               console.log(1);
+               if (placedDot && selectedDot) {
+                  drawMarker(context, placedDot.x, placedDot.y, 15, 'green');
+               }
             }
          };
-         if (context) {
-            context.beginPath();
-            context.arc(668, 521, 5, 0, 2 * Math.PI); // 5 is the radius of the dot
-            context.fillStyle = 'blue';
-            context.fill();
-         }
 
          img.src = selectedMap.displayIcon;
          if (img.complete) {
             canvas.width = img.width * 2;
             canvas.height = img.height * 2;
             context?.drawImage(img, 0, 0, img.width * 2, img.height * 2);
-            if (clickPosition && context) {
-               context.beginPath();
-               context.arc(
-                  clickPosition.x,
-                  clickPosition.y,
-                  10,
-                  0,
-                  2 * Math.PI,
-                  false,
-               );
-               context.fillStyle = 'red';
-               context.fill();
-            }
          }
       }
+   }, [maps, state.mapName, clickPosition, setClickPosition, hoverPosition]);
 
-      return () => {
-         window.removeEventListener('resize', handleResize);
-      };
-   }, [maps, state.mapName, clickPosition]);
+   const handleMouseMove = (
+      e: React.MouseEvent<HTMLCanvasElement, MouseEvent>,
+   ) => {
+      const canvas = canvasRef.current;
+      if (canvas) {
+         const rect = canvas.getBoundingClientRect();
+         const scaleX = canvas.width / rect.width;
+         const scaleY = canvas.height / rect.height;
+
+         const x = (e.clientX - rect.left) * scaleX;
+         const y = (e.clientY - rect.top) * scaleY;
+
+         setHoverPosition({ x, y });
+      }
+   };
+
+   const drawMarker = (
+      context: CanvasRenderingContext2D,
+      x: number,
+      y: number,
+      radius: number = 15,
+      color: string = 'blue',
+   ) => {
+      context.beginPath();
+      context.arc(x, y, radius, 0, 2 * Math.PI);
+      context.fillStyle = color;
+      context.fill();
+   };
 
    const handleClick = async (
       e: React.MouseEvent<HTMLCanvasElement, MouseEvent>,
@@ -120,14 +154,47 @@ const ValorantMode: React.FC<ValorantModeProps> = ({ state, dispatch }) => {
          const x = (e.clientX - rect.left) * scaleX;
          const y = (e.clientY - rect.top) * scaleY;
 
-         setClickPosition({ x, y });
+         // Check if click is within 15 pixels of a dot
+         const clickedDot = coordinates.find(
+            (coord) => Math.hypot(coord.x - x, coord.y - y) < 15,
+         );
 
-         try {
-            const response = await axios.post('/save-coordinates', { x, y });
-            console.log(response.data);
-         } catch (error) {
-            console.error(error);
+         const mapName = state.mapName as keyof typeof mapData;
+
+         if (selectedDot && clickedDot && selectedDot === clickedDot) {
+            // If the same dot was clicked, show all dots
+            console.log(mapData[mapName].coordinates);
+            setCoordinates(mapData[mapName].coordinates);
+            setSelectedDot(null);
+            setPlacedDot(null);
+         } else if (clickedDot) {
+            // If a dot was clicked, only show that dot
+            setCoordinates([clickedDot]);
+            setSelectedDot(clickedDot);
+         } else if (selectedDot) {
+            // If a dot was selected and the clicked position is not transparent, place a new dot
+            const context = canvas.getContext('2d');
+            const imageData = context?.getImageData(x, y, 1, 1).data;
+            if (imageData && imageData[3] !== 0) {
+               setPlacedDot({ x, y });
+               dispatch({
+                  type: 'setLineupLocationCoords',
+                  payload: {
+                     x: selectedDot.x,
+                     y: selectedDot.y,
+                     name: selectedDot.name,
+                  },
+               });
+               if (placedDot) {
+                  dispatch({
+                     type: 'setLineupPositionCoords',
+                     payload: { x: placedDot.x, y: placedDot.y },
+                  });
+               }
+            }
          }
+
+         setClickPosition({ x, y });
       }
    };
 
@@ -136,13 +203,14 @@ const ValorantMode: React.FC<ValorantModeProps> = ({ state, dispatch }) => {
          <select
             id="mapName"
             value={state.mapName}
-            onChange={(e) =>
+            onChange={(e) => {
+               const mapName = e.target.value as keyof typeof mapData;
                dispatch({
                   type: 'setMapName',
-                  payload: e.target.value,
-               })
-               
-            }
+                  payload: mapName,
+               });
+               setCoordinates(mapData[mapName].coordinates);
+            }}
             className="flex text-black items-center w-full px-5 py-4 mb-5 mr-2 text-sm font-medium outline-none focus:bg-grey-400 placeholder:text-grey-700 bg-[#edf2f7] text-dark-grey-900 rounded-2xl"
          >
             <option value="">--</option>
@@ -159,18 +227,18 @@ const ValorantMode: React.FC<ValorantModeProps> = ({ state, dispatch }) => {
          </select>
 
          {selectedMap && (
-            <div className="map-container flex flex-col items-center justify-center m-auto">
                <canvas
                   ref={canvasRef}
                   onClick={handleClick}
+                  onMouseMove={handleMouseMove}
                   style={{
-                      width: '100%',
-                      maxWidth: '100vw',
-                      margin: '0 auto',
-                      display: 'block',
+                     width: '100%',
+                     maxWidth: '100vw',
+                     margin: '0 auto',
+                     display: 'block',
                   }}
                />
-            </div>
+            
          )}
 
          <label
