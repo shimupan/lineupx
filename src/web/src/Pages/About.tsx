1a73aa900fc4451bd2ac164b79e011b802c2e767
Derek Lee <37049496+Kagiri2@users.noreply.github.com>
2024-02-03 18:00:58 -0500

    Update About.tsx

--- src/web/src/Pages/About.tsx
+++ src/web/src/Pages/About.tsx
@@ -2,19 +2,55 @@ import { Header, Footer, SideNavWrapper } from '../Components';
 
 const About: React.FC = () => {
    return (
-      <>
+      <div
+         style={{
+            display: 'flex',
+            flexDirection: 'column',
+            minHeight: '100vh',
+         }}
+      >
          <Header />
 
          <SideNavWrapper />
-         <div className=" text-white p-8 sm:pl-8 md:pl-32 lg:pl-64">
-               <h1 className="text-4xl font-bold text-blue-500 mb-4">About LineupX</h1>
+         <div
+            className="text-white p-8 sm:pl-8 md:pl-64 lg:pl-64 text-sm md:text-base lg:text-lg"
+            style={{ maxWidth: '1500px', flex: '1' }}
+         >
+            <div
+               className="text-white p-8 sm:pl-8 md:pl-64 lg:pl-64 text-sm md:text-base lg:text-lg"
+               style={{ maxWidth: '1500px' }}
+            >
+               <h1 className="text-4xl font-bold text-blue-500 mb-4">
+                  About LineupX
+               </h1>
                <p className="mb-4">
-                 Welcome to <span className="text-pink-500">LineupX</span>, a community-driven, open-source project dedicated to enhancing the gaming experience for players around the globe...
+                  <span className="text-pink-500">LineupX</span> is a
+                  community-driven open-source project dedicated to enhancing
+                  the gaming experience for players around the globe. Our focus
+                  is on lineups, a key element of many FPS games, and we aim to
+                  provide a platform for gamers to share and learn from each
+                  other. Although lineups aren't required to play FPS games they
+                  are a neccesary component if you want to maximize your
+                  potential. We currently support Valorant and CS2 but plan to
+                  expand to other games in the future.
                </p>
-               {/* Continue with the content, utilizing Tailwind CSS for styling */}
+
+               <h1 className="text-4xl font-bold text-blue-500 mb-4">
+                  Our Vision
+               </h1>
+               <p className="mb-4">
+                  Our mission is to provide a platform where gamers can learn
+                  and share lineups rapidly. Normally when you want to learn a
+                  lineup you would have to watch a video or read a guide, but
+                  with LineupX you can learn them in three pictures. We believe
+                  that by using three pictures (where you stand to throw the
+                  lineup, crosshair placement, and where the lineup lands) players
+                  can learn lineups faster. 
+               </p>
             </div>
+         </div>
          <Footer />
-      </>
+      </div>
    );
 };
 
