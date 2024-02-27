e123472a0bdc1c9d793262ccc6b8a82c827ed0b6
Kagiri2 <37049496+Kagiri2@users.noreply.github.com>
2024-02-27 16:59:07 -0500

    refactor

--- src/web/src/Pages/Page.tsx
+++ src/web/src/Pages/Page.tsx
@@ -1,6 +1,5 @@
-// Main page of the app
 import { useContext, useEffect, useRef } from 'react';
-import { Header, Game, Footer, SideNavWrapper } from '../Components';
+import { Header, Game, Footer, SideNavWrapper, BackgroundGradientAnimation } from '../Components';
 import { AuthContext } from '../App';
 import { ToastContainer, toast } from 'react-toastify';
 
@@ -23,6 +22,7 @@ const GREETINGS = [
 const Page: React.FC = () => {
    const Auth = useContext(AuthContext);
    const initialRender = useRef(true);
+
    useEffect(() => {
       if (initialRender.current) {
          initialRender.current = false;
@@ -38,18 +38,18 @@ const Page: React.FC = () => {
 
    return (
       <>
-         <Header />
-
-         <SideNavWrapper />
-
-         <div className="h-screen flex">
-            <div className="main-content flex-col md:flex-row flex-1 flex justify-center items-center space-x-4">
-               <Game game={'game/CS2'} name={cs2} />
-               <Game game={'game/Valorant'} name={valorant} />
+         <BackgroundGradientAnimation />
+         <div className="relative z-10">
+            <Header />
+            <SideNavWrapper />
+            <div className="h-screen flex">
+               <div className="main-content flex-col md:flex-row flex-1 flex justify-center items-center space-x-4">
+                  <Game game={'game/CS2'} name={cs2} />
+                  <Game game={'game/Valorant'} name={valorant} />
+               </div>
             </div>
+            <Footer />
          </div>
-
-         <Footer />
 
          <ToastContainer
             position="top-center"
