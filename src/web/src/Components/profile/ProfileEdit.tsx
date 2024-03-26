87b944fdb59eef8fcfc2812da935b8b12d895990
Kagiri2 <37049496+Kagiri2@users.noreply.github.com>
2024-03-26 16:18:58 -0400

    Update ProfileEdit.tsx

--- src/web/src/Components/profile/ProfileEdit.tsx
+++ src/web/src/Components/profile/ProfileEdit.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { UserType } from '../../global.types';
 import { IoMdClose } from 'react-icons/io';
 import { ToastContainer, toast } from 'react-toastify';
@@ -18,14 +18,14 @@ const ProfileEdit: React.FC<ProfileEditProps> = ({ user, setOpen }) => {
       const id = toast.loading('Saving Changes...');
       if (newUsername === user.username && newEmail === user.email) {
          toast.update(id, {
-            render: 'Username and Email are the same',
-            type: 'error',
+            render: 'No changes detected.',
+            type: 'info',
             isLoading: false,
-            autoClose: 1000,
-            hideProgressBar: false,
+            autoClose: 2000,
          });
          return;
       }
+
       axios
          .post('/user/update', {
             user: user,
@@ -35,81 +35,77 @@ const ProfileEdit: React.FC<ProfileEditProps> = ({ user, setOpen }) => {
          .then((response) => {
             toast.update(id, {
                render: response.data.message,
-               type: response.data.type,
+               type: 'success',
                isLoading: false,
-               autoClose: 1000,
-               hideProgressBar: false,
+               autoClose: 2000,
             });
          })
          .catch((error) => {
             toast.update(id, {
-               render: error.response.data.message,
-               type: error.response.data.type,
+               render: error.response
+                  ? error.response.data.message
+                  : 'An error occurred',
+               type: 'error',
                isLoading: false,
-               autoClose: 1000,
-               hideProgressBar: false,
+               autoClose: 2000,
             });
          });
    }
 
    return (
-      <>
-         <div className="flex justify-center mt-20 px-8">
-            <form className="max-w-2xl" onSubmit={handleSubmit}>
-               <div className="flex flex-wrap border shadow rounded-lg p-3 dark:bg-gray-600">
-                  <div className="flex justify-between w-full text-xl text-gray-600 dark:text-gray-300 pb-2">
-                     <h2>Account Settings:</h2>
-                     <IoMdClose
-                        size={20}
-                        className="cursor-pointer"
-                        onClick={() => {
-                           setOpen(!open);
-                        }}
-                     />
-                  </div>
-
-                  <div className="flex flex-col gap-2 w-full border-gray-400">
-                     <div>
-                        <label className="text-gray-600 dark:text-gray-400">
-                           User Name: {user.username}
-                        </label>
-                        <input
-                           className="w-full py-3 border border-slate-200 rounded-lg px-3 focus:outline-none focus:border-slate-500 hover:shadow dark:bg-gray-600 dark:text-gray-100"
-                           type="text"
-                           placeholder="Enter your username (leave blank for default)"
-                           onChange={(e) => {
-                              setNewUsername(e.target.value);
-                           }}
-                        />
-                     </div>
-
-                     <div>
-                        <label className="text-gray-600 dark:text-gray-400">
-                           Email: {user.email}
-                        </label>
-                        <input
-                           className="w-full py-3 border border-slate-200 rounded-lg px-3 focus:outline-none focus:border-slate-500 hover:shadow dark:bg-gray-600 dark:text-gray-100"
-                           type="text"
-                           placeholder="Enter your email (leave blank for default)"
-                           onChange={(e) => {
-                              setNewEmail(e.target.value);
-                           }}
-                        />
-                     </div>
-                     <div className="flex justify-end">
-                        <button
-                           className="py-1.5 px-3 m-1 text-center bg-violet-700 border rounded-md text-white  hover:bg-violet-500 hover:text-gray-100 dark:text-gray-200 dark:bg-violet-700"
-                           type="submit"
-                        >
-                           Save changes
-                        </button>
-                     </div>
-                  </div>
+      <div className="fixed inset-0 bg-black bg-opacity-30 flex justify-center items-center">
+         <div className="bg-white rounded-lg shadow-xl p-6 w-full max-w-lg">
+            <div className="flex justify-between items-center">
+               <h2 className="text-xl text-gray-700 font-semibold">
+                  Edit Profile
+               </h2>
+               <IoMdClose
+                  className="cursor-pointer"
+                  onClick={() => setOpen(false)}
+               />
+            </div>
+            <button onClick={() => setOpen(false)} className="block text-sm font-medium text-gray-700">Back to Profile Page</button>
+            <form onSubmit={handleSubmit} className="mt-4">
+               <div className="mb-4">
+                  <label
+                     htmlFor="username"
+                     className="block text-sm font-medium text-gray-700"
+                  >
+                     New Username
+                  </label>
+                  <input
+                     type="text"
+                     id="username"
+                     value={newUsername}
+                     onChange={(e) => setNewUsername(e.target.value)}
+                     className="text-gray-700 mt-1 p-2 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-500 focus:ring-opacity-50"
+                  />
                </div>
+               <div className="mb-4">
+                  <label
+                     htmlFor="email"
+                     className="block text-sm font-medium text-gray-700"
+                  >
+                     Confirm your Email
+                  </label>
+                  <input
+                     type="email"
+                     id="email"
+                     value={newEmail}
+                     onChange={(e) => setNewEmail(e.target.value)}
+                     className="text-gray-700 mt-1 p-2 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-500 focus:ring-opacity-50"
+                  />
+               </div>
+               <button
+                  type="submit"
+                  className="w-full bg-blue-600 text-white p-2 rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-4 focus:ring-blue-500 focus:ring-opacity-50"
+               >
+                  Save Changes
+               </button>
             </form>
          </div>
          <ToastContainer position="top-center" />
-      </>
+      </div>
    );
 };
 
