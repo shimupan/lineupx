2c3ccb8531189c97adc2366a6e8e6ef51f0b05de
shimupan <shimupan@users.noreply.github.com>
2024-07-13 21:14:44 +0000

    Prettified Code!

--- src/api/routes/index.js
+++ src/api/routes/index.js
@@ -1,5 +1,5 @@
-    export { default as auth } from './auth.js';
-    export { default as user } from './user.js';
-    export { default as post } from './post.js';
-    export { default as comment } from './comment.js';
-    export { default as replies } from './replies.js';
+export { default as auth } from './auth.js';
+export { default as user } from './user.js';
+export { default as post } from './post.js';
+export { default as comment } from './comment.js';
+export { default as replies } from './replies.js';
