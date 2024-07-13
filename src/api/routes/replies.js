a5d54e15cb5afe0bae54100f449bb77bc3422038
shimupan <shimupan@gmail.com>
2024-07-13 17:27:56 -0400

    added filename to the commit message

--- src/api/routes/replies.js
+++ src/api/routes/replies.js
@@ -1,9 +1,9 @@
-import express from 'express';
+                import express from 'express';
 
-const router = express.Router();
+                const router = express.Router();
 
-router.get('/replies', (req, res) => {});
+                router.get('/replies', (req, res) => {});
 
-router.post('/replies', (req, res) => {});
+                router.post('/replies', (req, res) => {});
 
-export default router;
+                export default router;
--- src/api/server.js
+++ src/api/server.js
@@ -1,46 +1,46 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors';
-import { mongo } from './config/index.js';
-import { auth, user, post, comment, replies } from './routes/index.js';
-import session from 'express-session';
-import passport from 'passport';
-import mongoose from 'mongoose';
-import { updateCS2Posts, updateValorantPosts } from './helper/updatePosts.js';
+      import express from 'express';
+      import dotenv from 'dotenv';
+      import cors from 'cors';
+      import { mongo } from './config/index.js';
+      import { auth, user, post, comment, replies } from './routes/index.js';
+      import session from 'express-session';
+      import passport from 'passport';
+      import mongoose from 'mongoose';
+      import { updateCS2Posts, updateValorantPosts } from './helper/updatePosts.js';
 
-dotenv.config();
+      dotenv.config();
 
-mongo();
+      mongo();
 
-const app = express();
-app.use(cors());
+      const app = express();
+      app.use(cors());
 
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb', extended: true }));
-app.use(express.json());
+      app.use(express.json({ limit: '50mb' }));
+      app.use(express.urlencoded({ limit: '50mb', extended: true }));
+      app.use(express.json());
 
-app.get('/', (req, res) => {
-   res.send('server is running');
-});
+      app.get('/', (req, res) => {
+         res.send('server is running');
+      });
 
-app.use(
-   session({
-      secret: 'mrpopo',
-      resave: false,
-      saveUninitialized: false,
-   }),
-);
+      app.use(
+         session({
+            secret: 'mrpopo',
+            resave: false,
+            saveUninitialized: false,
+         }),
+      );
 
-app.use(auth);
-app.use(user);
-app.use(post);
-app.use(comment);
-app.use(replies);
-app.use(passport.initialize());
-app.use(passport.session());
+      app.use(auth);
+      app.use(user);
+      app.use(post);
+      app.use(comment);
+      app.use(replies);
+      app.use(passport.initialize());
+      app.use(passport.session());
 
-const PORT = process.env.PORT || 3000; // Use environment variable for port or default to 3000
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+      const PORT = process.env.PORT || 3000; // Use environment variable for port or default to 3000
+      app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
-// await updateCS2Posts();
-// await updateValorantPosts();
+      // await updateCS2Posts();
+      // await updateValorantPosts();
