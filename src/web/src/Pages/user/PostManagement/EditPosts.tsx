8ae52ef45d62b82822af7657aec46bdb628c2f0a
GitHub Action <action@github.com>
2024-07-22 03:52:55 +0000

    Prettify Code: src/web/src/Pages/user/PostManagement/EditPosts.tsx

--- src/web/src/Pages/user/PostManagement/EditPosts.tsx
+++ src/web/src/Pages/user/PostManagement/EditPosts.tsx
@@ -262,7 +262,7 @@ const EditPost: React.FC = () => {
                      <button
                         type="submit"
                         className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                        disabled={isLoading} 
+                        disabled={isLoading}
                      >
                         Update Post
                      </button>
