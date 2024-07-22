75101c9f75e4885ea6f6947970c918c429738029
Derek Lee <37049496+Kagiri2@users.noreply.github.com>
2024-07-21 20:38:51 -0400

    Update ManagePosts.tsx

--- src/web/src/Pages/user/PostManagement/ManagePosts.tsx
+++ src/web/src/Pages/user/PostManagement/ManagePosts.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useContext } from 'react';
+import { useEffect, useState, useContext } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import {
    Header,
