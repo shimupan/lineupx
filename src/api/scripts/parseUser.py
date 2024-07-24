ff8a34ce6ee8eb5676344c32978df8c7b3df7079
shimupan <shimupan@gmail.com>
2024-07-24 02:30:45 -0400

    updated users

--- src/api/scripts/parseUser.py
+++ src/api/scripts/parseUser.py
@@ -9,9 +9,9 @@ with open(file_path, 'r') as file:
     data = json.load(file)
 
 # Modify each entry
-for entry in data:  # Assuming 'data' is a list of dictionaries
-    entry['password'] = ""
-    entry['email'] = ""
+for index, entry in enumerate(data, start=1):  # Assuming 'data' is a list of dictionaries
+    entry['password'] = "f{1}"
+    entry['email'] = f"{index}@gmail.com"
     entry['username'] = names.get_full_name()
 
 # Write the modified data back to the file
