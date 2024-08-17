8303e46089e414fe8f0ff0f5d0b87fe1994103ff
shimupan <shimupan@gmail.com>
2024-08-17 00:25:17 -0400

    testing auto env create

---
+++ scripts/setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+check_and_copy_env() {
+  local dir=$1
+  cd $dir || exit 1
+
+  if [ ! -f .env ]; then
+    cp .env.example .env
+
+  cd - > /dev/null || exit 1
+}
+
+# Navigate to src/api and check .env
+check_and_copy_env "src/api"
+
+# Navigate to src/web and check .env
+check_and_copy_env "src/web"
