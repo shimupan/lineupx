5737e372fcceac5933f77a218ce51a9af99d46ed
GitHub Action <action@github.com>
2024-10-07 00:37:56 +0000

    Prettify Code: src/web/src/hooks/useUserCache.ts

--- src/web/src/hooks/useUserCache.ts
+++ src/web/src/hooks/useUserCache.ts
@@ -6,38 +6,35 @@ const useUserCache = () => {
    const [userCache, setUserCache] = useState<Record<string, UserType>>({});
    const fetchingUsers = useRef<Set<string>>(new Set());
 
-   const fetchUsers = useCallback(
-      async (userIds: string[]) => {
-         const uniqueUserIds = [...new Set(userIds)];
-         const usersToFetch = uniqueUserIds.filter(
-            (id) => !userCache[id] && !fetchingUsers.current.has(id),
-         );
+   const fetchUsers = useCallback(async (userIds: string[]) => {
+      const uniqueUserIds = [...new Set(userIds)];
+      const usersToFetch = uniqueUserIds.filter(
+         (id) => !userCache[id] && !fetchingUsers.current.has(id),
+      );
 
-         if (usersToFetch.length > 0) {
-            usersToFetch.forEach((id) => fetchingUsers.current.add(id));
-            try {
-               const newUsers = await getUsersByIDs(usersToFetch);
-               const newUsersRecord = newUsers.reduce(
-                  (acc: Record<string, UserType>, user: UserType) => {
-                     acc[user._id] = user;
-                     return acc;
-                  },
-                  {},
-               );
+      if (usersToFetch.length > 0) {
+         usersToFetch.forEach((id) => fetchingUsers.current.add(id));
+         try {
+            const newUsers = await getUsersByIDs(usersToFetch);
+            const newUsersRecord = newUsers.reduce(
+               (acc: Record<string, UserType>, user: UserType) => {
+                  acc[user._id] = user;
+                  return acc;
+               },
+               {},
+            );
 
-               setUserCache((prevCache) => ({
-                  ...prevCache,
-                  ...newUsersRecord,
-               }));
-            } catch (error) {
-               console.error('Error fetching users:', error);
-            } finally {
-               usersToFetch.forEach((id) => fetchingUsers.current.delete(id));
-            }
+            setUserCache((prevCache) => ({
+               ...prevCache,
+               ...newUsersRecord,
+            }));
+         } catch (error) {
+            console.error('Error fetching users:', error);
+         } finally {
+            usersToFetch.forEach((id) => fetchingUsers.current.delete(id));
          }
-      },
-      [],
-   );
+      }
+   }, []);
 
    return { userCache, fetchUsers };
 };
