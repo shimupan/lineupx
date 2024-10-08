0abfb87083bbcbbda8a6170d033cdae48ba7be20
Yuyang Zheng <136272059+yuyzheng@users.noreply.github.com>
2024-10-08 17:02:57 -0400

    added menu for mobile and icons for the menu (#341)
    * added menu for mobile and icons for the menu
    
    * Prettify Code: src/web/src/Components/global/bottombar/BottomNav.tsx
    src/web/src/Components/global/bottombar/Menu.tsx
    
    ---------
    
    Co-authored-by: root <root@MSI>
    Co-authored-by: GitHub Action <action@github.com>

---
+++ src/web/src/Components/global/bottombar/Menu.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import grenade from '../../../assets/svg/grenade.svg';
+import molly from '../../../assets/svg/molly.svg';
+
+const IconWrapper = ({ children }: { children: React.ReactNode }) => (
+   <div className="w-6 h-6 flex items-center justify-center">{children}</div>
+);
+
+const Menu: React.FC = () => {
+   const navigate = useNavigate();
+   const handleClick = (item: string) => {
+      navigate(item);
+   };
+
+   return (
+      <div className="absolute mt-2 w-48 bg-white shadow-lg rounded">
+         <ul className="py-2">
+            <li
+               className="px-4 py-2 flex items-center justify-center"
+               onClick={() => handleClick('/game/CS2/Lineups')}
+            >
+               <IconWrapper>
+                  <img src={grenade} alt="Lineups" className="w-6 h-6 invert" />
+               </IconWrapper>
+               <span className="px-4 py-2 text-black text-center">
+                  CS2 Lineups
+               </span>
+            </li>
+            <li
+               className="px-4 py-2 flex items-center justify-center"
+               onClick={() => handleClick('/game/Valorant/Agents')}
+            >
+               <IconWrapper>
+                  <img src={molly} alt="Lineups" className="w-6 h-6 invert" />
+               </IconWrapper>
+               <span className="px-4 py-2 text-black text-center">
+                  Val Lineups
+               </span>
+            </li>
+         </ul>
+      </div>
+   );
+};
+
+export default Menu;
