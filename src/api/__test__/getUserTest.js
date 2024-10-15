1166caa6fa2e73bb624fd7fd3e81fdaf95d4683c
GitHub Action <action@github.com>
2024-10-15 21:27:52 +0000

    Prettify Code: src/api/__test__/getUserTest.js

--- src/api/__test__/getUserTest.js
+++ src/api/__test__/getUserTest.js
@@ -1,10 +1,10 @@
 // Use jest to test code!
 import request from 'supertest';
-import server from '../server.js'; 
+import server from '../server.js';
 
 describe('Testing Get User', () => {
-  it('should return data from the database', async () => {
-    const res = await request(server).get('/user/ooccupate');
-    expect(res.username).toBe("ooccupate");
-  });
+   it('should return data from the database', async () => {
+      const res = await request(server).get('/user/ooccupate');
+      expect(res.username).toBe('ooccupate');
+   });
 });
