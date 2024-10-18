c7b52e2c954764d76afd8ac7be81063efcb1f787
GitHub Action <action@github.com>
2024-10-18 20:24:45 +0000

    Prettify Code: src/jest.config.js

--- src/jest.config.js
+++ src/jest.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  testMatch: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[tj]s?(x)"],
-  testPathIgnorePatterns: ["/node_modules/"],
+   testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[tj]s?(x)'],
+   testPathIgnorePatterns: ['/node_modules/'],
 };
