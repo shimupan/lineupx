ade9b3122693bcd3e454cdee2939a4a52737e0ed
haokingluo <hao5k6032@gmail.com>
2024-10-25 17:56:26 -0400

    [RSO] Start /account/me GET endpoint
    Created an endpoint in auth.js that will fetch a GET to Riot's
    /account endpoint to know who logged in using RSO and get the
    player's userid, player tag, and player name.

--- src/api/routes/test.js
+++ src/api/routes/test.js
@@ -5,7 +5,7 @@ const router = express.Router();
 router.get('/test', async (req, res) => {
    try {
       console.log("test endpoint");
-      res.status(200).json({ message: 'All good' });
+      res.status(200).json({ message: 'All good', expires_in: 600, "access_token": "qeweqrwr", "refresh_token": "rrretetett" });
    }
    catch (error){
       console.error('error', error);
