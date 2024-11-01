5eda58fc767d595866916b099151e1b00c4d374b
Yuyang Zheng <136272059+yuyzheng@users.noreply.github.com>
2024-11-01 16:25:49 -0400

    349 add searchbar to manage posts (#381)
    * added search bar with no functionality to manage post
    
    * created a search bar for the manage posts page that searches all the posts
    
    * Prettify Code: src/web/src/Components/game/Searchbar.tsx
    src/web/src/Pages/user/PostManagement/ManagePosts.tsx
    src/web/src/Pages/user/PostManagement/SearchPost.tsx
    
    * fixing bool and string variable issues
    
    * Prettify Code: src/web/src/Pages/user/PostManagement/EditPosts.tsx
    
    * removing unused variables
    
    ---------
    
    Co-authored-by: root <root@MSI>
    Co-authored-by: GitHub Action <action@github.com>

---
+++ src/web/src/Pages/user/PostManagement/SearchPost.tsx
@@ -0,0 +1,76 @@
+import React, { useState, FormEvent, ChangeEvent } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Fuse from 'fuse.js';
+import { PostType } from '../../../global.types';
+
+interface SearchBarProps {
+   onSearch: (searchTerm: string) => void;
+   game: string;
+   placeholder: string;
+   className?: string;
+   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+   suggestions: string[];
+   post: PostType[];
+   searchTerm: string;
+}
+
+const SearchPost = ({
+   onSearch,
+   placeholder,
+   className = '',
+   onChange,
+   game,
+   post,
+   searchTerm,
+}: SearchBarProps) => {
+   const [_, setFilteredSuggestions] = useState<string[]>([]);
+   const navigate = useNavigate();
+
+   const handleSubmit = (event: FormEvent) => {
+      event.preventDefault();
+      onSearch(searchTerm);
+      const sanitizedSearchTerm = searchTerm.replace(/\//g, '');
+      navigate(`/search/${game}/${sanitizedSearchTerm}`);
+   };
+
+   const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+      onChange(event);
+
+      const value = event.target.value;
+      if (value.trim() === '') {
+         setFilteredSuggestions([]);
+         return;
+      }
+
+      // Setup Fuse.js for fuzzy search
+      const fuse = new Fuse(post, {
+         keys: ['postTitle'],
+         includeScore: true,
+         threshold: 0.3,
+         isCaseSensitive: false,
+      });
+
+      // Perform the search with Fuse.js
+      const result = fuse.search(value);
+      const filtered = result.map(({ item }) => item.postTitle);
+
+      setFilteredSuggestions(filtered);
+   };
+
+   return (
+      <div className={`p-4 ${className}`}>
+         <form onSubmit={handleSubmit}>
+            <input
+               type="text"
+               value={searchTerm}
+               onChange={handleChange}
+               placeholder={placeholder}
+               className="text-black h-11/12 w-full p-2 border border-gray-800 rounded-md"
+               autoComplete="on"
+            />
+         </form>
+      </div>
+   );
+};
+
+export default SearchPost;
