1860388b64672e718ccfb69d6e95ea9c758356fe
Chris <gorgorchrislo@gmail.com>
2024-12-07 10:57:55 -0500

    Update to layout of page. Changed text size, adding padding and weights.

--- src/web/src/Pages/user/ProfileSearch.tsx
+++ src/web/src/Pages/user/ProfileSearch.tsx
@@ -43,71 +43,73 @@ const ProfileSearch: React.FC = () => {
             </div>
          </div>
 
-         {/* PFP and username */}
-         <div className="flex items-center mb-8">
-            <img
-               src="https://pbs.twimg.com/media/FgsO2EkaYAEivnn.jpg"
-               alt="Player"
-               className="rounded-full w-20 h-20 mr-6"
-            />
-            <div className="text-white">
-               <h2 className="text-2xl font-semibold">fractioned</h2>
+         <div className="px-20">
+            {/* PFP and username */}
+            <div className="flex items-center mb-8">
+               <img
+                  src="https://pbs.twimg.com/media/FgsO2EkaYAEivnn.jpg"
+                  alt="Player"
+                  className="rounded-full w-20 h-20 mr-6"
+               />
+               <div className="text-white">
+                  <h2 className="text-2xl font-semibold">fractioned</h2>
+               </div>
             </div>
-         </div>
 
-         {/* Profile body */}
-         <div className="flex gap-8 mb-8">
-            <div className="bg-gray-900 p-6 rounded-lg shadow-md flex-1">
-               <p className="text-sm">Current Rating</p>
-               <h3 className="text-gray-500 text-xl font-semibold mt-2">
-                  Radiant
-               </h3>
+            {/* Profile body */}
+            <div className="flex gap-8 mb-8">
+               <div className="bg-gray-900 p-6 rounded-lg shadow-md flex-1">
+                  <p className="text-xl">Current Rating</p>
+                  <h3 className="text-gray-500 text-sm font-semibold mt-2">
+                     Radiant
+                  </h3>
+               </div>
+               <div className="bg-gray-900 p-6 rounded-lg shadow-md flex-[4]">
+                  <p className="text-xl">Overview</p>
+                  <h3 className="text-gray-500 text-sm font-semibold mt-2">
+                     Wins
+                  </h3>
+               </div>
             </div>
-            <div className="bg-gray-900 p-6 rounded-lg shadow-md flex-1">
-               <p className="text-sm">Overview</p>
-               <h3 className="text-gray-500 text-xl font-semibold mt-2">
-                  Wins
-               </h3>
-            </div>
-         </div>
 
-         <div className="flex gap-8 mb-8">
-            <div className="bg-gray-900 p-6 rounded-lg shadow-md flex-1">
-               <p className="text-sm">Accuracy</p>
-               <h3 className="text-gray-500 text-xl font-semibold mt-2">
-                  Head
-               </h3>
-               <h3 className="text-gray-500 text-xl font-semibold mt-2">
-                  Body
-               </h3>
-               <h3 className="text-gray-500 text-xl font-semibold mt-2">
-                  Legs
-               </h3>
+            <div className="flex gap-8 mb-8">
+               <div className="bg-gray-900 p-6 rounded-lg shadow-md flex-1">
+                  <p className="text-xl">Accuracy</p>
+                  <h3 className="text-gray-500 text-sm font-semibold mt-2">
+                     Head
+                  </h3>
+                  <h3 className="text-gray-500 text-sm font-semibold mt-2">
+                     Body
+                  </h3>
+                  <h3 className="text-gray-500 text-sm font-semibold mt-2">
+                     Legs
+                  </h3>
+               </div>
+               <div className="bg-gray-900 p-6 rounded-lg shadow-md flex-[4]">
+                  <p className="text-xl">Top Agents</p>
+                  <h3 className="text-gray-500 text-sm font-semibold mt-2">
+                     Agent
+                  </h3>
+               </div>
             </div>
-            <div className="bg-gray-900 p-6 rounded-lg shadow-md flex-1">
-               <p className="text-sm">Top Agents</p>
-               <h3 className="text-gray-500 text-xl font-semibold mt-2">
-                  Agent
-               </h3>
-            </div>
-         </div>
 
-         <div className="bg-gray-900 p-6 rounded-lg shadow-md">
-            <h3 className="text-2xl font-semibold mb-4">Match History</h3>
-            <div className="flex justify-between py-2 border-b border-gray-200">
-               <span>Ascent</span>
-               <span className="text-gray-500">Kills: 25 | K/D: 1.5</span>
-               <span className="text-green-500">Win</span>
-            </div>
-            <div className="flex justify-between py-2 border-b border-gray-200">
-               <span>Breeze</span>
-               <span className="text-gray-500">Kills: 18 | K/D: 1.0</span>
-               <span className="text-red-500">Loss</span>
-            </div>
-            <div className="flex justify-between py-2 border-b border-gray-200">
-               <span>Bind</span>
-               <span className="text-gray-500">Kills: 20 | K/D: 1.2</span>
-               <span className="text-green-500">Win</span>
+            <div className="bg-gray-900 p-6 rounded-lg shadow-md">
+               <h3 className="text-2xl font-semibold mb-4">Match History</h3>
+               <div className="flex justify-between py-2 border-b border-gray-200">
+                  <span>Ascent</span>
+                  <span className="text-gray-500">Kills: 25 | K/D: 1.5</span>
+                  <span className="text-green-500">Win</span>
+               </div>
+               <div className="flex justify-between py-2 border-b border-gray-200">
+                  <span>Breeze</span>
+                  <span className="text-gray-500">Kills: 18 | K/D: 1.0</span>
+                  <span className="text-red-500">Loss</span>
+               </div>
+               <div className="flex justify-between py-2 border-b border-gray-200">
+                  <span>Bind</span>
+                  <span className="text-gray-500">Kills: 20 | K/D: 1.2</span>
+                  <span className="text-green-500">Win</span>
+               </div>
             </div>
          </div>
       </div>
