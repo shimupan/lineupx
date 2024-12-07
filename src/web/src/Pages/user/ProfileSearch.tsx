f5f30b1e6581650db3e2fbbf8dd8cd6e433a85ef
Chris <gorgorchrislo@gmail.com>
2024-12-07 11:44:26 -0500

    Added second line of stats for Overview section.

--- src/web/src/Pages/user/ProfileSearch.tsx
+++ src/web/src/Pages/user/ProfileSearch.tsx
@@ -90,7 +90,7 @@ const ProfileSearch: React.FC = () => {
                </div>
                <div className="bg-gray-900 p-6 rounded-lg shadow-md flex-[4]">
                   <p className="text-xl">E9: A3 Competitive Overview</p>
-                  <div className="flex items-start gap-40 mt-2">
+                  <div className="flex items-start gap-40 mt-5">
 
                      <div className="border-l-4 border-gray-500 pl-2">
                         <h3 className="text-gray-500 text-base font-semibold">Wins</h3>
@@ -115,6 +115,35 @@ const ProfileSearch: React.FC = () => {
                      <div className="border-gray-500 pl-2">
                         <h3 className="text-gray-500 text-base font-semibold">Deaths</h3>
                         <h3 className="text-white text-xl font-semibold">2,271</h3>
+                     </div>
+
+                  </div>
+
+                  <div className="flex items-start gap-40 mt-5">
+
+                     <div className="border-gray-500 pl-2">
+                        <h3 className="text-gray-500 text-base font-semibold">Assists</h3>
+                        <h3 className="text-white text-xl font-semibold">982</h3>
+                     </div>
+
+                     <div className="border-l-4 border-gray-500 pl-2">
+                        <h3 className="text-gray-500 text-base font-semibold">ACS</h3>
+                        <h3 className="text-white text-xl font-semibold">229.2</h3>
+                     </div>
+
+                     <div className="border-gray-500 pl-2">
+                        <h3 className="text-gray-500 text-base font-semibold">KAD Ratio</h3>
+                        <h3 className="text-white text-xl font-semibold">1.68</h3>
+                     </div>
+
+                     <div className="border-gray-500 pl-2">
+                        <h3 className="text-gray-500 text-base font-semibold">Kills/Round</h3>
+                        <h3 className="text-white text-xl font-semibold">0.8</h3>
+                     </div>
+
+                     <div className="border-gray-500 pl-2">
+                        <h3 className="text-gray-500 text-base font-semibold">First Bloods</h3>
+                        <h3 className="text-white text-xl font-semibold">290</h3>
                      </div>
 
                   </div>
